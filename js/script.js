f5df5960ed0cbe025209d3546af33495bcb80afe
Beata Karolczak <karolczak.beata@gmail.com>
2017-12-22 00:08:13 +0100

    Add comments into js

---
+++ js/script.js
@@ -0,0 +1,14 @@
+// alert('test');
+
+var name = prompt('Enter your name');
+/* alert('Hello, ' + name); */
+console.log('Hello, ' + name);
+
+// One line comment could not be longer than the one you are reading right now 
+/* here
+I 
+can 
+have 
+several 
+line
+with comments */
